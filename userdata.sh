cb05f69d1665985117fd928feca74101adb7e1d8
jonas <jonasberg034@gmail.com>
2023-03-26 22:01:21 +0200

    userdata.sh modified

--- userdata.sh
+++ userdata.sh
@@ -9,3 +9,4 @@ usermod -a -G docker ec2-user
 curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64  \
 -o /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
