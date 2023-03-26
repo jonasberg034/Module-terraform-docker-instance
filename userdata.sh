b8de121b382092be1e8b618bd31c83084c90e59b
jonas <jonasberg034@gmail.com>
2023-03-26 15:00:24 +0200

    userdata modified

--- userdata.sh
+++ userdata.sh
@@ -6,6 +6,6 @@ systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
 # install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
+curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64  \
 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
